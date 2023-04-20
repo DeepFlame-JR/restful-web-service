22c8a97ccead8cd5993c1e1412008655b5bf9f06
JunRyeol <wnsfuf0121@naver.com>
2023-04-20 15:18:14 +0900

    [REST API] Initialize Project

---
+++ my-restful-services/src/main/java/com/example/myrestfulservices/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.example.myrestfulservices;
+
+// lombok 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+
+@Data  // 모든 API를 설정해줌
+@AllArgsConstructor  // 생성자(setter, getter)가 자동으로 생성됨
+public class HelloWorldBean {
+    private String message;  
+}
---
+++ my-restful-services/src/main/java/com/example/myrestfulservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.myrestfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (end-point)
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");  // json 타입으로 반환됨
+    }
+}
